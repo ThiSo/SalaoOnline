143bbc9a6576c1a9cbaebf43d4271bee9f2a63a4
ThiSo <81988524+ThiSo@users.noreply.github.com>
2023-08-13 18:47:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DB_NAME = "database.db"
 SECRET_KEY = 'carazinho'
 
 # Producao
-#DATABASE_URL = "postgresql://salaoonline_user:vLkCiPFYWEXDUd3M9DVeNYYBrNXRdkJU@dpg-ciou026nqql4qa0jg33g-a.oregon-postgres.render.com/salaoonline"
+DATABASE_URL = "postgresql://salaoonline_user:vLkCiPFYWEXDUd3M9DVeNYYBrNXRdkJU@dpg-ciou026nqql4qa0jg33g-a.oregon-postgres.render.com/salaoonline"
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,10 +13,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
+# SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
 
 # Connect to production database
-#SQLALCHEMY_DATABASE_URI = DATABASE_URL
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
